c7074b5e6d8c5b29daac06d1d1da04eaef0428b5
Natalia Pravosudova <58364090+NataliaPravosudova@users.noreply.github.com>
2023-01-16 15:48:20 +0100

    check lab5

--- lab5/script-lab5.sh
+++ lab5/script-lab5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-topic_arn=$(aws sns create-topic --name load_balancer_health --query 'TopicArn' --output text)
+topic_arn=$(aws sns create-topic --name lb_health --query 'TopicArn' --output text)
 subscribe_arn=$(aws sns subscribe --topic-arn $topic_arn --protocol email --notification-endpoint natalia19022002@gmail.com --query 'SubscriptionArn' --output text)
 aws cloudwatch put-metric-alarm --alarm-name WARNalarm --alarm-description "Instance in target group is deleted" --metric-name HealthyHostCount \
---namespace AWS/ApplicationELB --statistic Average --period 10 --threshold 1 --comparison-operator LessThanThreshold --dimensions \
-Name=TargetGroup,Value=targetgroup/lab4-tg/274e4b548d06b20d Name=LoadBalancer,Value=app/lab4-lb/f10804edc8ff41da --evaluation-periods 1 --alarm-actions $topic_arn --insufficient-data-actions $topic_arn
+--namespace AWS/ApplicationELB --statistic Average --period 60 --threshold 2 --comparison-operator LessThanThreshold --dimensions \
+Name=TargetGroup,Value=targetgroup/lab4-tg/4ab6f5a1254ea003 Name=LoadBalancer,Value=app/lab4-lb/885759b40d8cf99c --evaluation-periods 1 --alarm-actions $topic_arn --insufficient-data-actions $topic_arn
 echo "Done"
